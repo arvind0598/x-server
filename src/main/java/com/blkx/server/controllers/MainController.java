43ad746407c2d489a90aa38fd06d87383a077a8e
arnabpaul <arnabpaul313@gmail.com>
2020-03-23 02:26:45 +0530

    Supported single where clause + order by clause
    Will remove commented code later

--- src/main/java/com/blkx/server/controllers/MainController.java
+++ src/main/java/com/blkx/server/controllers/MainController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class MainController {
@@ -102,62 +104,121 @@ public class MainController {
     @PostMapping("/generate")
     public ResponseModel generateAPI(@RequestBody List<GenerateRequestModel> body) {
         ResponseModel response = new ResponseModel();
-
-        Map<String, String> groupedData = body.stream()
-                .collect(Collectors.groupingBy(
-                        GenerateRequestModel::getTableName,
-                        Collectors.mapping(GenerateRequestModel::getColumnName, Collectors.joining(" "))
-                )
-        );
-
-        Map<String, List<GenerateRequestModel>> group = body.stream()
-                .collect(Collectors.groupingBy(GenerateRequestModel::getTableName));
-
-        group = group.entrySet().stream()
-                .map(entry -> {
-                    List<GenerateRequestModel> models = entry.getValue();
-                    String columns = models.stream().map(GenerateRequestModel::getColumnName).collect(Collectors.joining(" "));
-                    String query = String.format("{ %s }", columns);
-
-                    Map<String, String> data = 
-                })
-
-         String query = group.entrySet().stream()
-                    .map(entry -> {
-                        List<GenerateRequestModel> models = entry.getValue();
-                        return models.stream()
-                                .map(model -> {
-                                    String option = model.getOption();
-                                    String field = model.getField();
-                                    String value = model.getValue();
-                                    String columnName = model.getColumnName();
-                                    switch(option){
-
-                                        //(where: {id: {_eq: 1}})
-                                        case "where": return String.format("%s( where: { %s: {_eq: %s}}){ %s }", entry.getKey(), columnName, field, );
-
-                                        //(order_by: {id: asc})
-                                        case "order_by": return String.format("%s ( order_by: { %s: %s}){ %s }", entry.getKey(),field, value, entry.getValue());
-
-                                        //(limit: 2)
-                                        case "limit": return String.format("%s ( limit: %s}){ %s }", entry.getKey(),value, entry.getValue());
-
-                                        default:
-                                    }
-                                })
-
-
-
-
-                        return String.format("%s { %s }", entry.getKey(), entry.getValue());
-                    })
-                    .collect(Collectors.joining(" "));
-
-            query = String.format("{ \"query\":  \"{ %s }\" }", query);
-//            query = String.format("{ \"query\": (%s : \"{\" %s : %s \"}\") \"{ %s }\" }", clause[0], clause[1], clause[2], query);
+        String conditions = "", attributes, finalQuery = "";
+        Map<String, String> queryMapCond = new HashMap<>();
+        Map<String, String> queryMapAtt = new HashMap<>();
+//        for(GenerateRequestModel g: body) {
+//            queryMapCond.put(g.getTableName(), String.format("%s", g.getTableName()));
 //        }
 
-        UUID uuid = configService.insertNewQuery(query);
+        for(GenerateRequestModel g: body){
+            if(g.getOption()!= null)
+            {
+                switch (g.getOption()) {
+                    case "where":
+                        conditions = String.format("(where: {%s:{_eq: %s}})", g.getColumnName(), g.getValue());
+                        break;
+                    case "order_by":
+                        conditions = String.format("(order_by: {%s: %s})", g.getColumnName(), g.getValue());
+                        break;
+                }
+            }
+            else{
+                conditions = "";
+            }
+            queryMapCond.put(g.getTableName(),conditions);
+        }
+
+        for(GenerateRequestModel g: body){
+
+            attributes = String.format(" %s ", g.getColumnName());
+            if (!queryMapAtt.containsKey(g.getTableName()))
+            {
+                queryMapAtt.put(g.getTableName(), attributes);
+            }
+            else{
+                queryMapAtt.put(g.getTableName(), queryMapAtt.get(g.getTableName()).concat(attributes));
+            }
+        }
+
+        for(Map.Entry<String, String> entry : queryMapAtt.entrySet())
+        {
+            String tempQuery = String.format("%s %s { %s } %s", entry.getKey(), queryMapCond.get(entry.getKey()), entry.getValue(), "\n");
+            finalQuery = finalQuery.concat(tempQuery);
+        }
+
+
+
+//        Map<String, String> groupedData = body.stream()
+//                .collect(Collectors.groupingBy(
+//                        GenerateRequestModel::getTableName,
+//                        Collectors.mapping(GenerateRequestModel::getColumnName, Collectors.joining(" "))
+//                )
+//        );
+//
+//        Map<String, List<GenerateRequestModel>> group = body.stream()
+//                .collect(Collectors.groupingBy(GenerateRequestModel::getTableName));
+//
+////        group = group.entrySet().stream()
+////                .map(entry -> {
+////                    List<GenerateRequestModel> models = entry.getValue();
+////                    String columns = models.stream().map(GenerateRequestModel::getColumnName).collect(Collectors.joining(" "));
+////                    String query = String.format("{ %s }", columns);
+////
+//////                    Map<String, String> data =
+////                })
+//
+//         String query = group.entrySet().stream()
+//                    .map(entry -> {
+//                        List<GenerateRequestModel> models = entry.getValue();
+//                        final int[] flag = {0};
+//                        Stream<Object> qu = models.stream()
+//                                .map(model -> {
+//                                    String option = model.getOption();
+//                                    String field = model.getField();
+//                                    String value = model.getValue();
+//                                    String columnName = model.getColumnName();
+//                                    String q = "";
+//                                    if (flag[0] == 1 && option.equals("none"))
+//                                        option = "randomPlaceholder";
+//                                    switch (option) {
+//
+//                                        case "none":
+//                                            q = String.format("%s{ %s", entry.getKey(), columnName);
+//                                            flag[0] = 1;
+//                                            break;
+//
+//                                            //(where: {id: {_eq: 1}})
+//                                        case "where":
+//                                            q = String.format("%s( where: { %s: {_eq: %s}}){ %s ", entry.getKey(), columnName, value, columnName);
+//                                            break;
+//
+//                                            //(order_by: {id: asc})
+////                                        case "order_by":
+////                                            q = String.format("%s ( order_by: { %s: %s}){ %s ", entry.getKey(), field, value, entry.getValue());
+////
+//                                            //(limit: 2)
+////                                        case "limit":
+////                                            q = String.format("%s ( limit: %s}){ %s ", entry.getKey(), value, entry.getValue());
+//
+//                                        default:
+//                                            q = String.format("%s %s", q, columnName);
+//                                    }
+//
+//                                    return  q;
+//                                });
+//
+//                        return String.format("%s }", qu);
+////                        return String.format("%s { %s }", entry.getKey(), entry.getValue());
+//                    })
+//                    .collect(Collectors.joining(" "));
+//
+            ////            query = String.format("{ \"query\": (%s : \"{\" %s : %s \"}\") \"{ %s }\" }", clause[0], clause[1], clause[2], query);
+////        }
+
+        finalQuery = String.format("{ \"query\":  \"{ %s }\" }", finalQuery);
+
+        UUID uuid = configService.insertNewQuery(finalQuery);
         response.setSuccess(true);
         response.setMessage(ResponseMessage.ADD_SUCCESS.toString());
         response.setData(uuid.toString());
